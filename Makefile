7917de9f25f9beec04725353baff36cc52e0917c
Hi120ki <12624257+hi120ki@users.noreply.github.com>
2022-10-28 10:17:18 +0900

    init
    Signed-off-by: Hi120ki <12624257+hi120ki@users.noreply.github.com>

---
+++ Makefile
@@ -0,0 +1,8 @@
+all: help
+
+help: ## Print this help message
+	@grep -E '^[a-zA-Z._-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+.PHONY: ping
+ping: ## ping
+	http http://localhost:8080/ping
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := r.Run(); err != nil {
+		panic(err)
+	}
+}
